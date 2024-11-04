0afda0fd8eae37187da181dfef9c47638eca9ddd
Resseett <cgiler@alumnos.uai.cl>
2024-11-03 22:33:50 -0300

    arreglo

---
+++ proyecto/app/Componentes/LanguageContext.js
@@ -0,0 +1,35 @@
+// Componentes/LanguageContext.js
+
+import React, { createContext, useState, useEffect } from 'react';
+
+// Crear el contexto
+export const LanguageContext = createContext();
+
+// Crear el proveedor del contexto
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState('es'); // Idioma por defecto: Español
+
+  // Cargar el idioma desde Local Storage al montar el componente
+  useEffect(() => {
+    const storedLanguage = localStorage.getItem('preferredLanguage');
+    if (storedLanguage) {
+      setLanguage(storedLanguage);
+    }
+  }, []);
+
+  // Guardar el idioma en Local Storage cuando cambia
+  useEffect(() => {
+    localStorage.setItem('preferredLanguage', language);
+  }, [language]);
+
+  // Función para cambiar el idioma
+  const toggleLanguage = () => {
+    setLanguage((prevLang) => (prevLang === 'es' ? 'en' : 'es'));
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage, toggleLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
