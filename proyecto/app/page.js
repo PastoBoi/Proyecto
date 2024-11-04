50c2f2b6cd826069bff69d61baba131c16d20bfe
Resseett <cgiler@alumnos.uai.cl>
2024-11-03 23:10:38 -0300

    arreglo

--- proyecto/app/page.js
+++ proyecto/app/page.js
@@ -1,11 +1,15 @@
-// /app/page.js
+// app/page.js
 
 "use client";
 
 import { useRouter } from 'next/navigation';
+import { useContext } from 'react';
+import { LanguageContext } from '../Componentes/LanguageContext';
+import translations from '../Componentes/traducción';
 
 export default function HomePage() {
   const router = useRouter();
+  const { language } = useContext(LanguageContext);
 
   const handleLoginRedirect = () => {
     router.push('/login');
@@ -15,22 +19,34 @@ export default function HomePage() {
     router.push('/register');
   };
 
+  // Función para traducir textos
+  const t = (key) => {
+    return translations[language][key] || key;
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[url('https://www.concierto.cl/wp-content/uploads/2018/09/Obsolete_CDs.jpg')]">
+    <div
+      className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: "url('https://www.concierto.cl/wp-content/uploads/2018/09/Obsolete_CDs.jpg')",
+      }}
+    >
       <div className="box-border bg-blue-300 items-center flex flex-col p-4 rounded-xl">
-        <h2 className="text-3xl font-semibold mt-4 text-blue-900">¡Bienvenido a Sustainable Sound!</h2>
+        <h2 className="text-3xl font-semibold mt-4 text-blue-900">
+          {t('welcome')}
+        </h2>
         <div className="mt-4 text-2xl">
-          <button 
-            onClick={handleLoginRedirect} 
+          <button
+            onClick={handleLoginRedirect}
             className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 mr-2"
           >
-            Iniciar Sesión
+            {t('login')}
           </button>
-          <button 
-            onClick={handleRegisterRedirect} 
+          <button
+            onClick={handleRegisterRedirect}
             className="bg-green-600 text-white py-2 px-4 rounded hover:bg-green-700"
           >
-            Crear Usuario
+            {t('createUser')}
           </button>
         </div>
       </div>
